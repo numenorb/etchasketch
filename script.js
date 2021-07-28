4297c5e931237ad03f0233b58acd2fa0f41c02b9
Matthew Breig <matthew.breig@gmail.com>
2021-07-28 18:11:12 -0400

    added rainbow function and toggle

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const sketchBox = document.querySelector("#sketch-box");
 const grid = document.querySelector("#grid");
 const resetButton = document.querySelector("#reset-btn");
 const selectedColor = document.querySelector("#color-choice");
+const rainbowButton = document.querySelector("#rainbow");
 let size = document.querySelector("#size");
+let rainbowStatus = false;
 
 createGrid(makeChoice("#size"));
 
@@ -20,8 +22,16 @@ function createGrid(dim){
 const gridList = document.querySelectorAll(".grid-box"); // NodeList of createGrid(dim)
 
 function changeColor(e){
-    e.target.style.backgroundColor= makeChoice("#color-choice");
+    if (rainbowStatus){
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        e.target.style.backgroundColor = "#" + randomColor;
+    } else{
+        e.target.style.backgroundColor = makeChoice("#color-choice");
+    }
 }
+
+
+
 
 function makeChoice(element){  //element should be entered with CSS notation
     let newElement = document.querySelector(element).value;
@@ -34,7 +44,7 @@ function resetGrid(){ //to run after rest is clicked
         grid.removeChild(grid.firstChild);
     }
     createGrid(makeChoice("#size")); //already has value from makeChoice
-    gridList.forEach(gridList => gridList.style.backgroundColor = 'rgb(200, 200, 200)');
+    gridList.forEach(gridList => gridList.style.backgroundColor = 'rgb(200, 200, 200,.4)');
     
 }
 
@@ -46,19 +56,20 @@ function btnTest(){
 resetButton.onclick = function(){resetGrid()};
 
 size.addEventListener('mouseup', function() {resetGrid();});
+rainbowButton.onclick = function(){
+    if (rainbowStatus){
+        rainbowButton.classList.toggle("rainbow-active");
+        rainbowStatus = false;
+    } else{
+        rainbowButton.classList.toggle("rainbow-active");
+        rainbowStatus = true;       
+    }
+    console.log(rainbowStatus);
+}
 
 
 /*  PSUEDO CODE
-    Be able to create a grid of 16x16 units
-    - done - createGrid(dim)
-    Can change color of square
-    - done - added to createGrid(dim) & chageColor(e)
-    Hovering over a square changes the colors of that square
-    - done
-    Create a button that: clears the current styling on the grid
-    Create option for choosing size (max 100x100)
-    - use buttons and prompts
-    - works if you change size, clears old and puts in new
+    
     add toggle/scale that allows random color and grayscale
     - grayscale is 10% dark, go over it a second time, and 
     - add 10% more, until full
