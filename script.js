ee1262b34f219095ca8ff38022c93481e43d8219
Matthew Breig <matthew.breig@gmail.com>
2021-07-30 22:43:44 -0400

    Got grayscale to work

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const grid = document.querySelector("#grid");
 const resetButton = document.querySelector("#reset-btn");
 const selectedColor = document.querySelector("#color-choice");
 const rainbowButton = document.querySelector("#rainbow");
+const grayscaleButton = document.querySelector("#grayscale");
+const colorButton = document.querySelector("#color-choice-btn");
 let size = document.querySelector("#size");
-let rainbowStatus = false;
+
+// maybe color,grayscale & rainbow should share one status
+let colorStatus = 0; // 0= color, 1=rainbow, 2=grayscale
 
 createGrid(makeChoice("#size"));
 
 function createGrid(dim){
     grid.style.gridTemplateRows = `repeat(${dim}, 1fr)`;
     grid.style.gridTemplateColumns= `repeat(${dim}, 1fr)`;
-
     for (let i = 0; i < dim ** 2; i++){
         const gridBox = document.createElement("div");
+        gridBox.style.backgroundColor = 'rgba(0,0,0,0.0)';
         gridBox.classList.add("grid-box");
         gridBox.addEventListener("mouseover",changeColor);
         grid.append(gridBox);
@@ -22,14 +26,33 @@ function createGrid(dim){
 const gridList = document.querySelectorAll(".grid-box"); // NodeList of createGrid(dim)
 
 function changeColor(e){
-    if (rainbowStatus){
+    //console.log(`rain: ${rainbowStatus} grey: ${grayscaleStatus}`);
+    
+    if (colorStatus == 0){
+        e.target.style.backgroundColor = makeChoice("#color-choice");
+    }
+    if (colorStatus == 2){
         let randomColor = Math.floor(Math.random()*16777215).toString(16);
         e.target.style.backgroundColor = "#" + randomColor;
-    } else{
-        e.target.style.backgroundColor = makeChoice("#color-choice");
+    } 
+    if(colorStatus == 1){
+        let shade = e.target.style.backgroundColor.slice(-3,-1);
+        console.log("shade before: " + shade);
+        if(shade <= 0.89){
+            shade = Number(shade) + 0.1;
+            //console.log("shade after: " + shade);
+            //console.log("color? " + `rgba(0,0,0,${shade})`);
+            e.target.style.backgroundColor = `rgba(0,0,0,${shade})`;
+        } else{
+            e.target.style.backgroundColor = "black";
+        }
+
     }
 }
 
+function grayScale(e){
+
+}
 
 
 
@@ -44,7 +67,7 @@ function resetGrid(){ //to run after rest is clicked
         grid.removeChild(grid.firstChild);
     }
     createGrid(makeChoice("#size")); //already has value from makeChoice
-    gridList.forEach(gridList => gridList.style.backgroundColor = 'rgb(200, 200, 200,.4)');
+    gridList.forEach(gridList => gridList.style.backgroundColor = 'rgba(240,240,240,1.0))');
     
 }
 
@@ -55,17 +78,26 @@ function btnTest(){
 
 resetButton.onclick = function(){resetGrid()};
 
-size.addEventListener('mouseup', function() {resetGrid();});
-rainbowButton.onclick = function(){
-    if (rainbowStatus){
-        rainbowButton.classList.toggle("rainbow-active");
-        rainbowStatus = false;
-    } else{
-        rainbowButton.classList.toggle("rainbow-active");
-        rainbowStatus = true;       
-    }
-    console.log(rainbowStatus);
+function changeColorStatus (buttonStatus, defaultStatus){
+    rainbowButton.classList.toggle("rainbow-active");
+    grayscaleButton.classList.toggle('grayscale-active');
+   /* if (buttonStatus == 1){
+        let allGrid = document.querySelectorAll(".grid-box");
+        for (let i = 0; i < allGrid.length; i++){
+            allGrid[i].style.opacity=0;
+        }
+    }*/
+    // Trying something else to do the above first
+    return colorStatus != buttonStatus ? colorStatus = buttonStatus : colorStatus = defaultStatus; // if rainbow isn't on, turn it on, if its on, turn it to basic color 
 }
+
+
+//Button & Slider Operations
+size.addEventListener('mouseup', function() {resetGrid();});
+rainbowButton.onclick = function(){changeColorStatus(2,0)}; //changes between rainbow and color 
+grayscaleButton.onclick = function(){changeColorStatus(1,0)};//changes between gray and color
+colorButton.onclick = function(){changeColorStatus(0,0)};
+
 
 
 /*  PSUEDO CODE
